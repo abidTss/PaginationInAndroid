2b387bfc7db95d9a936d28c32ede495fb49f1ece
abidTss <abid.teastallstudio@gmail.com>
2017-10-23 17:53:37 +0530

    just init

---
+++ app/src/main/java/com/abid/paginationinandroid/MainActivity.java
@@ -0,0 +1,75 @@
+package com.abid.paginationinandroid;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private EndlessScrollListener scrollListener;
+    Context context=MainActivity.this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Configure the RecyclerView
+        RecyclerView rvItems = (RecyclerView) findViewById(R.id.recyclerviewPayment);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvItems.setLayoutManager(linearLayoutManager);
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(page);
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvItems.addOnScrollListener(scrollListener);
+    }
+
+    // Append the next page of data into the adapter
+    // This method probably sends out a network request and appends new data items to your adapter.
+    public void loadNextDataFromApi(int offset) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
+    }
+    class RecyclerViewAdapter extends RecyclerView.Adapter<MyHolder>{
+        LayoutInflater inflate;
+
+        public RecyclerViewAdapter() {
+            inflate = LayoutInflater.from(context);
+        }
+        @Override
+        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = inflate.inflate(R.layout.orderlist_sap_item, null);
+            return new MyHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+
+        public MyHolder(View itemView) {
+            super(itemView);
+        }
+    }
+    }
+
